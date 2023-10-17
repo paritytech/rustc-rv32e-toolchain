7c850ac89a89102d68b407b5c632482a05473072
gabriele-0201 <gabriele.miotti2@gmail.com>
2023-10-17 10:17:12 +0200

    test build linux and macOS

--- 04-build-dist.sh
+++ 04-build-dist.sh
@@ -2,11 +2,13 @@
 
 set -euo pipefail
 
+TOOLCHAIN_HOST_TRIPLET=$(rustc --version --verbose | grep 'host: ' | sed -r 's/host: (.*)/\1/')
+
 # Build dists
 cd rust
 # TODO: can this be remove?
 mkdir -p build/riscv32em-unknown-none-elf/compiler-doc
-./x dist rustc rust-dev rust-std cargo rust-src rustfmt clippy rustc-docs
+./x dist rustc rust-dev rust-std cargo rust-src rustfmt clippy rustc-docs --target $TOOLCHAIN_HOST_TRIPLET --target riscv32em-unknown-none-elf
 # The output of the compilation shoudl be in ../../rust/build/dist
 
 
@@ -14,13 +16,13 @@ mkdir -p build/riscv32em-unknown-none-elf/compiler-doc
 cd build/dist/
 ls
 tar -zcf dist_output.tar.gz \
-    rustc-nightly-x86_64-unknown-linux-gnu.tar.xz \
-    rust-dev-nightly-x86_64-unknown-linux-gnu.tar.xz \
-    rust-std-nightly-x86_64-unknown-linux-gnu.tar.xz \
+    rustc-nightly-$TOOLCHAIN_HOST_TRIPLET.tar.xz \
+    rust-dev-nightly-$TOOLCHAIN_HOST_TRIPLET.tar.xz \
+    rust-std-nightly-$TOOLCHAIN_HOST_TRIPLET.tar.xz \
     rust-std-nightly-riscv32em-unknown-none-elf.tar.xz \
-    cargo-nightly-x86_64-unknown-linux-gnu.tar.xz \
+    cargo-nightly-$TOOLCHAIN_HOST_TRIPLET.tar.xz \
     rust-src-nightly.tar.xz \
-    rustfmt-nightly-x86_64-unknown-linux-gnu.tar.xz \
-    clippy-nightly-x86_64-unknown-linux-gnu.tar.xz \
-    rustc-docs-nightly-x86_64-unknown-linux-gnu.tar.xz \
+    rustfmt-nightly-$TOOLCHAIN_HOST_TRIPLET.tar.xz \
+    clippy-nightly-$TOOLCHAIN_HOST_TRIPLET.tar.xz \
+    rustc-docs-nightly-$TOOLCHAIN_HOST_TRIPLET.tar.xz \
     rustc-docs-nightly-riscv32em-unknown-none-elf.tar.xz

45d704cb1294726851a76cd17f953dd8a6cf343a
gabriele-0201 <gabriele.miotti2@gmail.com>
2023-10-22 23:34:34 +0200

    test new toolchain_version_name

--- clone.sh
+++ clone.sh
@@ -2,8 +2,10 @@
 
 set -euo pipefail
 
+source config.sh -rust_commit
+
 git clone "https://github.com/rust-lang/rust.git"
 cd rust
 # This could change if the patch will be updated
-git checkout 8d321f7a88f0ae27793c133390e507bf1f49125a
+git checkout $RUST_COMMIT
 git submodule update --init --recursive
--- config.sh
+++ config.sh
@@ -1,18 +1,38 @@
 #!/usr/bin/env bash
 
-# TODO: this is executed BEFORE building rustc, this mean that
-# rustc is already installed on the machine if this work
+param1=${1:-}
+
+export RUST_COMMIT=8d321f7a88f0ae27793c133390e507bf1f49125a
+
+# If -rust_commit specified then only export the rust commit variable
+if [ "$param1" == "-rust_commit" ]; then
+  exit 0
+fi
 
 export TOOLCHAIN_HOST_TRIPLET=$(rustc --version --verbose | grep 'host: ' | sed -r 's/host: (.*)/\1/')
 
-# TODO: maybe this should be updated with `nightly-2023-04-05-r1`
-export TOOLCHAIN_VERSION=$(rustc --version --verbose | grep 'release: ' | sed -r 's/release: (.*)/\1/')-r$(cat release_number)
+cd rust
+git show --no-patch --format=%ci $RUST_COMMIT > commit_show_output
+commit_show_output=$(cat commit_show_output)
+rm commit_show_output
+cd ../
+
+regex='([0-9]{4}-[0-9]{2}-[0-9]{2})'
+if [[ $commit_show_output =~ $regex ]]; then
+  rust_commit_date="${BASH_REMATCH[1]}"
+else
+  echo "Impossible extracting date from rustc commit"
+  exit 1
+fi
+
+export TOOLCHAIN_VERSION=nightly-$rust_commit_date-r$(cat release_number)
 
 export TOOLCHAIN_NAME=riscv32em-$TOOLCHAIN_VERSION-$TOOLCHAIN_HOST_TRIPLET
 export ARTIFACT_NAME=rust-$TOOLCHAIN_NAME
-
-param1=${1:-}
+# test
+echo $ARTIFACT_NAME
 
 if [ "$param1" == "-artifact_name" ]; then
     echo "ARTIFACT_NAME=$ARTIFACT_NAME" >> $GITHUB_ENV
 fi
+

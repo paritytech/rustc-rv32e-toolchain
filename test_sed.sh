76eb68912e145eb684817dddc73c3aab33102f2a
gabriele-0201 <gabriele.miotti2@gmail.com>
2023-10-22 09:42:12 +0200

    Test build on with clean up manifests

--- test_sed.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "test: to_replace/to_replace/" > test-file1
-echo "test: to_replace/to_replace/" > test-file2
-sed -i'' -e 's#to_replace/to_replace/##g' test-*
-cat test-file1
-cat test-file2

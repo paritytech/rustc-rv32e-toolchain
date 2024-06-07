e9b8f74e067e70c899c27eded9422f74bb38afcf
Lane Rettig <lanerettig@gmail.com>
2024-06-07 13:41:41 -0700

    Add loweratomic flag and executables

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ source config.sh
 
 # Will create component archives (dists) ./rust/build/dist
 cd rust
-./x dist rustc rust-std cargo rust-src rustfmt clippy --target $TOOLCHAIN_HOST_TRIPLET,riscv32em-athena-zkvm-elf
+CARGO_TARGET_RISCV32EM_ATHENA_ZKVM_ELF_RUSTFLAGS="-Cpasses=loweratomic" ./x dist rustc rust-std cargo rust-src rustfmt clippy --target $TOOLCHAIN_HOST_TRIPLET,riscv32em-athena-zkvm-elf

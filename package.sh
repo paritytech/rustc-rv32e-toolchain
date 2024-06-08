5e61788c3f2f45ee61211eda681fcc32a21b41e6
Lane Rettig <lanerettig@gmail.com>
2024-06-07 17:00:59 -0700

    Simplify package and add new GHA workflows

--- package.sh
+++ package.sh
@@ -4,47 +4,6 @@ set -euo pipefail
 
 source config.sh
 
-# We generally remove files as soon as we don't need them
-# anymore to minimize disk space usage. Especially important for github runners
-# which don't have much space.
-
-inst () {
-    echo "Installing: $1"
-    tar -xf ../$1.tar.gz
-    # eager remove to reduce max disk space used
-    rm ../$1.tar.gz
-    cd $1
-    ./install.sh --disable-ldconfig --prefix="" --destdir="/tmp/$TOOLCHAIN_NAME"
-    cd ../
-}
-
-# dist contains the tared components we want to install
-mv rust/build/dist .
-#rm -rf rust
-
-cd dist
-mkdir unpack
-cd unpack
-
-inst "rustc-nightly-$TOOLCHAIN_HOST_TRIPLET"
-inst "rust-std-nightly-$TOOLCHAIN_HOST_TRIPLET"
-inst "rust-std-nightly-riscv32em-athena-zkvm-elf"
-inst "cargo-nightly-$TOOLCHAIN_HOST_TRIPLET"
-inst "rust-src-nightly"
-inst "rustfmt-nightly-$TOOLCHAIN_HOST_TRIPLET"
-inst "clippy-nightly-$TOOLCHAIN_HOST_TRIPLET"
-
-cd ..
-rm -rf unpack
-
-cd ../
-
-# remove the /tmp/ prefix from the manifest to make it path independent
-sed -i'' -e "s#/tmp/$TOOLCHAIN_NAME/##g" /tmp/$TOOLCHAIN_NAME/lib/rustlib/manifest-*
-rm -f /tmp/$TOOLCHAIN_NAME/lib/rustlib/install.log
-
-# the final toolchain tar that can just be extracted anywhere to install it
-tar --zstd -cf $ARTIFACT_NAME.tar.zst -C /tmp $TOOLCHAIN_NAME
-
-rm -rf dist
-rm -rf /tmp/$TOOLCHAIN_NAME
+TOOLCHAIN_DIR=rust/build/$TOOLCHAIN_HOST_TRIPLET/stage2
+cp -n rust/build/$TOOLCHAIN_HOST_TRIPLET/stage2-tools-bin/* $TOOLCHAIN_DIR/bin
+tar --exclude lib/rustlib/src --exclude lib/rustlib/rustc-src -hczvf rust-toolchain-$TOOLCHAIN_NAME.tar.gz -C $TOOLCHAIN_DIR .

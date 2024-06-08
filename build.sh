3cee681e6a6d1320632f97cdc4b1cb5b57b3166f
Lane Rettig <lanerettig@gmail.com>
2024-06-08 13:18:29 -0700

    Update config

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ source config.sh
 
 # Will create component archives (dists) ./rust/build/dist
 cd rust
-CARGO_TARGET_RISCV32EM_ATHENA_ZKVM_ELF_RUSTFLAGS="-Cpasses=loweratomic" ./x build
-CARGO_TARGET_RISCV32EM_ATHENA_ZKVM_ELF_RUSTFLAGS="-Cpasses=loweratomic" ./x build --stage 2
+CARGO_TARGET_RISCV32EM_ATHENA_ZKVM_ELF_RUSTFLAGS="-Cpasses=loweratomic" ./x build --target $TOOLCHAIN_HOST_TRIPLET,riscv32em-athena-zkvm-elf
+CARGO_TARGET_RISCV32EM_ATHENA_ZKVM_ELF_RUSTFLAGS="-Cpasses=loweratomic" ./x build --stage 2 --target $TOOLCHAIN_HOST_TRIPLET,riscv32em-athena-zkvm-elf

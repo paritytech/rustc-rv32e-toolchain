be8feed5090b166fa1dca2254cf0adbc5d5cb437
Lane Rettig <lanerettig@gmail.com>
2024-06-10 13:55:15 -0700

    Fix release artifact name

--- package.sh
+++ package.sh
@@ -6,4 +6,4 @@ source config.sh
 
 TOOLCHAIN_DIR=rust/build/$TOOLCHAIN_HOST_TRIPLET/stage2
 cp -n rust/build/$TOOLCHAIN_HOST_TRIPLET/stage2-tools-bin/* $TOOLCHAIN_DIR/bin
-tar --exclude lib/rustlib/src --exclude lib/rustlib/rustc-src -hczvf rust-toolchain-$TOOLCHAIN_NAME.tar.gz -C $TOOLCHAIN_DIR .
+tar --exclude lib/rustlib/src --exclude lib/rustlib/rustc-src -hczvf $ARTIFACT_NAME.tar.gz -C $TOOLCHAIN_DIR .

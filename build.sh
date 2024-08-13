9dce500bfae3b50eee09eb14d1274b37aa03778b
Lane Rettig <lanerettig@gmail.com>
2024-08-12 19:38:19 -0700

    Compilation is working again
    Include dependency on riscv toolchain
    Include small llvm patch

--- build.sh
+++ build.sh
@@ -4,6 +4,15 @@ set -euo pipefail
 
 source config.sh
 
+# Rust requires a custom target file to exist for our custom target as part of the bootstrap build,
+# but it doesn't actually look at the contents.
+touch /tmp/riscv32em-athena-zkvm-elf.json
+
 # Will create component archives (dists) ./rust/build/dist
 cd rust
-CARGO_TARGET_RISCV32EM_ATHENA_ZKVM_ELF_RUSTFLAGS="-Cpasses=loweratomic" ./x build --stage 2
+#RUSTFLAGS="-C link-arg=-L$HOME/compiler_rt_build_riscv32em-athena-zkvm-elf/lib/baremetal" \
+#LIBRARY_PATH="$HOME/compiler_rt_build_riscv32em-athena-zkvm-elf/lib/baremetal" \
+RUST_TARGET_PATH="/tmp" \
+CARGO_TARGET_RISCV32EM_ATHENA_ZKVM_ELF_RUSTFLAGS="-Cpasses=loweratomic" \
+CFLAGS_riscv32em_athena_zkvm_elf="-ffunction-sections -fdata-sections -fPIC -march=rv32em -mabi=ilp32e" \
+./x build --stage 2

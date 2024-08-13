c3ed1085218298675d282ba57ab0171dfcc543a4
Lane Rettig <lanerettig@gmail.com>
2024-08-12 19:47:43 -0700

    Enable macOS workflows

--- build.sh
+++ build.sh
@@ -10,8 +10,6 @@ touch /tmp/riscv32em-athena-zkvm-elf.json
 
 # Will create component archives (dists) ./rust/build/dist
 cd rust
-#RUSTFLAGS="-C link-arg=-L$HOME/compiler_rt_build_riscv32em-athena-zkvm-elf/lib/baremetal" \
-#LIBRARY_PATH="$HOME/compiler_rt_build_riscv32em-athena-zkvm-elf/lib/baremetal" \
 RUST_TARGET_PATH="/tmp" \
 CARGO_TARGET_RISCV32EM_ATHENA_ZKVM_ELF_RUSTFLAGS="-Cpasses=loweratomic" \
 CFLAGS_riscv32em_athena_zkvm_elf="-ffunction-sections -fdata-sections -fPIC -march=rv32em -mabi=ilp32e" \
